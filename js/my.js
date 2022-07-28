772ff943b3c0907ac04a31bf89eead33160ffa8b
hadeel <hadeeljamal@users.noreply.github.com>
2022-07-28 15:50:57 +0300

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,10 @@
+$("#myCarousel").carousel({
+  interval: 3000,
+});
+
+// $(document).ready(function(){
+//   $("#emphasis").click(function(){
+//     $("#MobileVerification").hide(0.4);
+//   });
+// });
+
